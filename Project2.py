ecd4ff8a3714000695611eae87d45d65ae7db09b
mkoper02 <maciej.koperdowski@gmail.com>
2023-11-21 19:56:21 +0100

    New data generation and new improved formula for calculating parameters

--- Project2.py
+++ Project2.py
@@ -1,81 +1,88 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Generate random samples from a normal distribution
-def generateInput(size):
-    return np.random.normal(0, 0.5, size)
-
 # Output based on set parameters and noise
 def calculateOutput(input_k, input_k1, input_k2, interferance):
-    b0_asterisk, b1_asterisk, b2_asterisk = 1, 1, 1
+    bs = [1, 1, 1]
 
-    return input_k * b0_asterisk + input_k1 * b1_asterisk + input_k2 * b2_asterisk + interferance
+    return input_k * bs[0] + input_k1 * bs[1] + input_k2 * bs[2] + interferance
 
-# Generate data (input, output) based on set parameters and noise
-def dataSimulation(time):
-    input = generateInput(len(time))
 
-    simulated_data = np.zeros((len(input), 2))
-    interferance_range = 2
-
-    for point in range(len(input)):
-        if point < 2:
-            simulated_data[point] = [input[point], 0]
-            continue
-
-        simulated_data[point] = [input[point], calculateOutput(input[point], input[point - 1], input[point - 2], np.random.uniform(-interferance_range / 2, interferance_range / 2))]
-
-    return simulated_data
-
-def createXnMatrix(input_data):
+# Generate random samples from a normal distribution and return as an array 
+def generateInput(size):
     row_size = 3
-
-    x_n = np.zeros((len(input_data) - 2, row_size))  
+    x = np.random.normal(0, 1.5, size)
+    x_n = np.zeros((size - 2, row_size))  
 
     for row in range(len(x_n)):
-        for i in range(row_size): x_n[row][i] = input_data[row + i]
+        for i in range(row_size): x_n[row][i] = x[row + i]
 
     return x_n
 
-def calculateAn(past_an, Pn, temp_xn, yn):
-    xn = np.array([temp_xn])
 
-    return np.array([past_an[:, 0] + (np.matmul(Pn, xn[0]) * (yn - (np.matmul(xn.T[:, 0],  past_an[:, 0]))))]).T
+# Generate output based on set parameters and noise
+def generateOutput(input):
+    output = np.zeros((len(input), 1))
+    interferance_range = 1
 
-def calculatePn(past_Pn, temp_xn, _lambda):
-    xn = np.array([temp_xn])
+    for point in range(len(input)):
+        output[point] = calculateOutput(input[point][0], input[point][1], input[point][2], np.random.uniform(-interferance_range / 2, interferance_range / 2))
 
-    # return past_Pn - (((np.matmul(np.matmul(past_Pn, xn[0]), xn.T) * past_Pn) / (_lambda + np.matmul(np.matmul(xn.T[:, 0], past_Pn), xn[0]))))
-    return (1 / _lambda) * (past_Pn - (((np.matmul(np.matmul(past_Pn, xn[0]), xn.T) * past_Pn) / (_lambda + np.matmul(np.matmul(xn.T[:, 0], past_Pn), xn[0])))))
+    return output
 
-def ex1(time):
-    simulated_data = dataSimulation(time)
 
-    Xn = createXnMatrix(simulated_data[:, 0]).T
-    Yn = simulated_data[2:, 1]
-    an = np.array([np.zeros(3)]).T
-    Pn = np.identity(3) * 10**5
+def wrmnk(Xn, Yn, _lambda):
+    Pn = np.identity(3) * 10 ** 5
+    b = np.ones((3, 1))
+    b_arr = []
 
-    an_arr = np.empty((Xn.shape[1], 3))
+    for x, y in zip(Xn, Yn):
+        x = np.array([x]).T
+        y = np.array([y])
 
-    for point in range(Xn.shape[1]):
-    # for point in range(4):
-        Pn = calculatePn(Pn, Xn[:, point], _lambda=1)
-        an = calculateAn(an, Pn, Xn[:, point], Yn[point])
-        an_arr[point] = an[:, 0]
+        numerator = Pn @ x @ x.T @ Pn
+        denominator = _lambda + (x.T @ Pn @ x)
+        Pn = (1 / _lambda) * (Pn - (numerator / denominator))
+        epsilon = y - np.dot(x.T, b)
+        b = b + (Pn @ x @ epsilon)
+        b_arr.append(b)
 
-    # plt.plot(time[2:], simulated_data[2:, 0])
-    # plt.plot(time[2:], an_arr[:, 0])
-    plt.plot(time[2:], an_arr)
-    plt.grid(); plt.show()
+    return np.array(b_arr)
+
 
 def main():
-    plot_range = 200    
-    numb_points = 5000
+    plot_range = 1000    
+    numb_points = 10000
 
     time = np.linspace(0, plot_range, num=numb_points)
+    Xn = generateInput(len(time))
+    Yn = generateOutput(Xn)    
 
-    ex1(time)
+    bs1 = wrmnk(Xn, Yn, _lambda=1)
+
+    # b0
+    plt.figure(figsize=(14, 8))
+    plt.subplots_adjust(left=0.06, right=0.95, top=0.95, bottom=0.06)
+    plt.plot(time[75 + 2:], [b[0] for b in bs1][75:])
+    plt.plot(time[75:], np.ones(len(time))[75:])
+    plt.grid(True); plt.ylabel(r'$b_{0}$'); plt.xlabel('Czas [s]')
+
+    # # b1
+    plt.figure(figsize=(14, 8))
+    plt.subplots_adjust(left=0.06, right=0.95, top=0.95, bottom=0.06)
+    plt.plot(time[75 + 2:], [b[1] for b in bs1][75:])
+    plt.plot(time[75:], np.ones(len(time))[75:])
+    plt.grid(True); plt.ylabel(r'$b_{1}$'); plt.xlabel('Czas [s]')
+    
+    # b2
+    plt.figure(figsize=(14, 8))
+    plt.subplots_adjust(left=0.06, right=0.95, top=0.95, bottom=0.06)
+    plt.plot(time[75 + 2:], [b[2] for b in bs1][75:])
+    plt.plot(time[75:], np.ones(len(time))[75:])
+    plt.grid(True); plt.ylabel(r'$b_{2}$'); plt.xlabel('Czas [s]')
+
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
