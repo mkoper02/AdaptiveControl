fa9ddcb4e8b327fa26520b1b4e2ea67a85220f59
Yetisio <jakub.piekarek@wp.pl>
2024-01-17 18:05:37 +0100

    Project3.py after correcting

--- Project3.py
+++ Project3.py
@@ -110,79 +110,64 @@ def plotCirclePoint(u1: float, u2 : float) -> None:
 
 
 def costFunction(U : np.ndarray, wanted : np.ndarray) -> float:
-    y1, y2 = calculateOutput(U, [0, 0]).T[0]
-    return (y1 - wanted[0]) ** 2 + (y2 - wanted[1]) ** 2
-
-def calculateOptimalU1(wanted):
-    l = -1
-    p = 1
-    epsilon = (abs(l) + abs(p)) / 100
-    for i in range(10):
-        u1 = (l + p) / 2
-        leftLimit = u1 - epsilon
-        rightLimit = u1 + epsilon
-        leftLimitU2 = math.sqrt(1 - pow(leftLimit, 2))
-        rightLimitU2 = math.sqrt(1 - pow(rightLimit, 2))
-        leftU = [leftLimit, calculateOptimalU2(-leftLimitU2, leftLimitU2, leftLimit, wanted)]
-        rightU = [rightLimit, calculateOptimalU2(-rightLimitU2, rightLimitU2, rightLimit, wanted)]
-        leftY = np.linalg.inv(np.identity(2) - (A_arr @ H_arr)) @ B_arr @ leftU
-        rightY = np.linalg.inv(np.identity(2) - (A_arr @ H_arr)) @ B_arr @ rightU
-        leftQ = pow((leftY[0] - wanted[0]), 2) + pow((leftY[1] - wanted[1]), 2)
-        rightQ = pow((rightY[0] - wanted[0]), 2) + pow((rightY[1] - wanted[1]), 2)
-        if leftQ > rightQ:
-            l = leftLimit
-        else:
-            p = rightLimit
-        epsilon = epsilon / 2
-    u1 = (l + p) / 2
-    return u1
+    Y = calculateOutput(U, [0, 0]).T[0]
+    return (Y[0] - wanted[0]) ** 2 + (Y[1]- wanted[1]) ** 2
 
 def calculateOptimalU2(L, P, U1, wanted):
-    l = L
-    p = P
-    epsilon = (abs(l) + abs(p)) / 100
-    for i in range(10):
-        u2 = (l + p) / 2
+    left_border = L
+    right_border = P
+    epsilon = (abs(left_border) + abs(right_border)) / 100
+    for i in range(30):
+        u2 = (left_border + right_border) / 2
         leftLimit = u2 - epsilon
         rightLimit = u2 + epsilon
         leftU = [U1, leftLimit]
         rightU = [U1, rightLimit]
-        leftY = np.linalg.inv(np.identity(2) - (A_arr @ H_arr)) @ B_arr @ leftU
-        rightY = np.linalg.inv(np.identity(2) - (A_arr @ H_arr)) @ B_arr @ rightU
-        leftQ = pow((leftY[0] - wanted[0]), 2) + pow((leftY[1] - wanted[1]), 2)
-        rightQ = pow((rightY[0] - wanted[0]), 2) + pow((rightY[1] - wanted[1]), 2)
-        if leftQ > rightQ:
-            l = leftLimit
+        leftQ = costFunction(np.array([leftU]).T, wanted)
+        rightQ = costFunction(np.array([rightU]).T, wanted)
+        if np.sum(leftQ) > np.sum(rightQ):
+            left_border = leftLimit
         else:
-            p = rightLimit
+            right_border = rightLimit
         epsilon = epsilon / 2
-    u2 = (l + p) / 2
+    u2 = (left_border + right_border) / 2
     return u2
 
+def calculateOptimalU1(wanted):
+    left_border = -RADIUS
+    right_border = RADIUS
+    epsilon = (abs(left_border) + abs(right_border)) / 100
+    for i in range(30):
+        u1 = (left_border + right_border) / 2
+        leftLimit = u1 - epsilon
+        rightLimit = u1 + epsilon
+        leftLimitU2 = np.sqrt(1 - leftLimit ** 2)
+        rightLimitU2 = np.sqrt(1 - rightLimit ** 2)
+        leftU = [leftLimit, calculateOptimalU2(-leftLimitU2, leftLimitU2, leftLimit, wanted)]
+        rightU = [rightLimit, calculateOptimalU2(-rightLimitU2, rightLimitU2, rightLimit, wanted)]
+        leftY = calculateOutput(np.array([leftU]).T, np.array([[0, 0]]).T)
+        rightY = calculateOutput(np.array([rightU]).T, np.array([[0, 0]]).T)   
+        leftQ = costFunction(np.array([leftU]).T, wanted)
+        rightQ = costFunction(np.array([rightU]).T, wanted)
+        if np.sum(leftQ) > np.sum(rightQ):
+            left_border = leftLimit
+        else:
+            right_border = rightLimit
+        epsilon = epsilon / 2
+    u1 = (left_border + right_border) / 2
+    return u1
+
 def optimizeU(wanted : np.ndarray) -> None:
-    # u_min = []
-    # u1=0
-    # optimal_u1 = calculateOptimalU1(u1,wanted)
-    # optimal_u2 = calculateOptimalU2(optimal_u1,wanted)
-    # cost = costFunction([optimal_u1,optimal_u2], wanted)
-    # print(f"Optymalne u1: {optimal_u1}")
-    # print(f"Optymalne u2: {optimal_u2}")
-    # print(f"Minimalna wartość funkcji kosztu: {cost}")
-
-    optU1 = calculateOptimalU1(wanted)
-    l = math.sqrt(1 - pow(optU1, 2))
-    optU2 = calculateOptimalU2(-l, l, optU1, wanted)
-
-    print("Optymalna wartosc U1: ", optU1, "\nOptymalna wartosc U2: ", optU2)
-    optU = [optU1, optU2]
-    Y = np.linalg.inv(np.identity(2) - (A_arr @ H_arr)) @ B_arr @ optU
-    quality = pow((Y[0] - 4), 2) + pow((Y[1] - 4), 2)
-    print("Kryterium jakosci: ", quality)
-
-    plotCirclePoint(optU1, optU2)
-
-
-
+    optimal_u1 = calculateOptimalU1(wanted)
+    left = math.sqrt(1 - pow(optimal_u1, 2))
+    right= -left
+    optimal_u2 = calculateOptimalU2(right, left, optimal_u1, wanted)
+    optimal_U = [optimal_u1, optimal_u2]
+    cost = costFunction(optimal_U, wanted)
+    print(f"Optymalne u1: {optimal_u1}")
+    print(f"Optymalne u2: {optimal_u2}")
+    print(f"Minimalna wartość funkcji kosztu: {cost}")
+    plotCirclePoint(optimal_u1, optimal_u2)
 
 def main() -> None:
     size = 1000
@@ -192,17 +177,6 @@ def main() -> None:
     # output = dataSimulation(size, noise_range)
     # parametersIdentification(output)
     optimizeU(wanted)
-
-
-def main() -> None:
-    size = 1000
-    noise_range = 0.1
-    wanted = np.array([4, 4])
-
-    # output = dataSimulation(size, noise_range)
-    # parametersIdentification(output)
-    optimizeU(wanted)
-
 
 if __name__ == '__main__':
     main()
